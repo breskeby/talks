691b6281217d2897b22cc02338de41749c475c2a
Travis CI <Rene@heartofgold.Speedport_W723_V_Typ_A_1_01_016>
2016-07-28 05:26:36 +0200

    simplify deploy

--- deploy.sh
+++ deploy.sh
@@ -9,23 +9,16 @@ function doBuild {
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doBuild
-    exit 0
-fi
+#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#    echo "Skipping deploy; just doing a build."
+#    doBuild
+#    exit 0
+#fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
-
-# Clone the existing gh-pages for this repo into gh-pages/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO gh-pages-trial
-cd gh-pages-trial
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
 
 # Clean out existing contents
 rm -rf gh-pages/**/* || exit 0
