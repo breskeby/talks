140eddf905ed2f6311c57a535b95348876fd21ff
Travis CI <Rene@heartofgold.Speedport_W723_V_Typ_A_1_01_016>
2016-07-28 05:28:01 +0200

    simplify deploy

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ function doBuild {
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-#    echo "Skipping deploy; just doing a build."
-#    doBuild
-#    exit 0
-#fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doBuild
+    exit 0
+fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -47,7 +47,6 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-chmod 600 deploy_key.enc
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
