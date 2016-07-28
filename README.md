6e0e0b8830e33e4d0a5c3a70af642f4b0aeb4486
Rene Groeschke <rene@gradle.com>
2016-07-28 03:58:33 +0200

    fix gradle invocation in deploy script

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/breskeby/talks.svg?branch=master)](https://travis-ci.org/breskeby/talks)
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages-trial"
 
 function doBuild {
-  ./gradlew.sh clean pages
+  ./gradlew clean pages
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
