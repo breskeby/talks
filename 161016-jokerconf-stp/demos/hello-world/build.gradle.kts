1f8ad00f661a11c39b5a3ecc798ea1d7824e1e6f
Travis CI <Rene@heartofgold.local>
2016-09-25 01:37:34 +0200

    update jc16 talk

---
+++ 161016-jokerconf-stp/demos/hello-world/build.gradle.kts
@@ -0,0 +1,23 @@
+
+import org.gradle.api.plugins.ApplicationPlugin
+import org.gradle.api.plugins.ApplicationPluginConvention
+import org.gradle.api.tasks.Copy
+import org.gradle.script.lang.kotlin.*
+
+apply<ApplicationPlugin>()
+
+configure<ApplicationPluginConvention>(){
+    mainClassName = "samples.HelloWorld"
+}
+
+task<Copy>("someCopy"){
+    from("")
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    testCompile("junit:junit:4.12")
+}
